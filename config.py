ad85f69c362b92bc8fac752ae957dbcbbf519603
razondirk <40529582+razondirk@users.noreply.github.com>
2020-07-03 00:40:11 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+PAPER_API_KEY = "Your Paper API Key Here"
+PAPER_SECRET_KEY = "Your Paper Secret Key Here"
+PAPER_BASE_URL = "https://paper-api.alpaca.markets"
+
+LIVE_API_KEY = "Your Live API Key Here"
+LIVE_SECRET_KEY = "Your Live Secret Key Here"
+LIVE_BASE_URL = "https://api.alpaca.markets"
