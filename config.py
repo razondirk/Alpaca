7e2284ae7ca896950a9a7b1abb1a8d57512d55f4
razondirk <40529582+razondirk@users.noreply.github.com>
2020-07-03 00:38:15 -0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-PAPER_API_KEY = "PKV6NDV7I00QIO7HS3DA"
-PAPER_SECRET_KEY = "MecMHyyeghQ59N/k007diLf5F87PmwmVvf2AsNSc"
-PAPER_BASE_URL = "https://paper-api.alpaca.markets"
-
-LIVE_API_KEY = "AKBZZC10R4S5E20GCTYJ"
-LIVE_SECRET_KEY = "fqExBFUBJYPFAaNcJkxNrbakIWlNcPt/x5DoFNMk"
-LIVE_BASE_URL = "https://api.alpaca.markets"
